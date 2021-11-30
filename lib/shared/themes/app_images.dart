be9e0648032081d4af965705c1cb855ebaef9741
Maria Vitoria de Jesus Damasceno <83039172+Maria-a17@users.noreply.github.com>
2021-11-30 09:53:00 -0300

    prmeiro comit

---
+++ lib/shared/themes/app_images.dart
@@ -0,0 +1,7 @@
+class  AppImages {
+static  const logoFull = "assets/images/Logotipo2.png";
+static  const logomini = "assets/images/Símbolo.png";
+static  const union = "assets/images/Union.png";
+static  const person = "assets/images/Image.png";
+//static  const google = "assets/images/google.png";
+}
